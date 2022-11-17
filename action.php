22e27feec04d492d402778e4c21676232420ae5d
Em_i <emiliencoss@gmail.com>
2022-11-17 23:56:33 +0100

    factorise regex verfication

--- action.php
+++ action.php
@@ -7,11 +7,17 @@ if (!isset($_GET["query"])) {
 $query = $_GET["query"];
 $queryAccepted = true;
 
-if (preg_match("/<script.*>.*<\/.*script.*>/", strtolower($query)))
-    $queryAccepted = false;
+$regexList = array("/<script.*>.*<\/.*script.*>/", "/order *:/", "/onerror *=/", "/onload *=/", "/onabort *=/", "/onanima.* *=/", "/onmouse.* *=/", "/while *(.*)/", "/location *=/");
+$reasonsQueryRefused = "<ul>";
 
-if (preg_match("/order *:/", strtolower($query)))
-    $queryAccepted = false;
+foreach ($regexList as $regex) {
+    if (preg_match($regex, strtolower($query))) {
+        $queryAccepted = false;
+        $reasonsQueryRefused .= "<li><code>" . htmlspecialchars($regex) . "</code></li>";
+    }
+}
+
+$reasonsQueryRefused .= "</ul>";
 
 if ($queryAccepted)
     if (!file_put_contents("content.txt", file_get_contents("content.txt") . "\n" . $query))
@@ -28,7 +34,7 @@ if ($queryAccepted)
 <body>
     <p>You sent the following content:</p>
     <code style="display: block;"><?=htmlspecialchars($query)?></code>
-    <?=$queryAccepted ? "<p>Your query has been accepted.</p>" : "<p><span style='color: red;'>Your query <b>has been rejected</b> because it doesn't comply with the rules.</span> Read the doc at <a href='manual'>/manual</a>.</p>"?>
+    <?=$queryAccepted ? "<p>Your query has been accepted.</p>" : "<p><span style='color: red;'>Your query <b>has been rejected</b> because it doesn't comply with the rules.</span> Read the doc at <a href='manual'>/manual</a>.</p><p>Paternes interdits :</p>" . $reasonsQueryRefused ?>
     <hr>
     <p><a href=".">Back to home</a></p>
 </body>
