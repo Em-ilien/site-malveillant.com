94ce22746821056ceef45dcbe7a72a04d66444ca
Em_i <emiliencoss@gmail.com>
2022-11-17 22:05:37 +0100

    manual update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <header>
         <div>
             <h1>site-malveillant.com</h1>
-            <p>Here you can publish content using XSS attack. Read the doc at <a href="manual">/manual</a>.</p>
+            <p>Here you can publish content using XSS attack. Read the doc at <a target="_blank" href="manual">/manual</a>.</p>
         </div>
         <form style="margin-bottom: 1em;" action="action.php">
             <input style="width: 30em; padding: 0.5em 1em; border-color: #888; border-radius: 5px;" type="text" name="query" placeholder="'Enter your<b>'; favorite query here --</b> name please'">
